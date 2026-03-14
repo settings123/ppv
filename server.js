8d4f9640f3954b22d79df6553910950bb22e8bc5
settings123 <32601410+settings123@users.noreply.github.com>
2026-03-14 18:35:12 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const puppeteer = require('puppeteer-core');
-const chromium = require('chrome-aws-lambda');
+const chromium = require('@sparticuz/chromium');
 const http = require('http');
 const https = require('https');
 const path = require('path');
